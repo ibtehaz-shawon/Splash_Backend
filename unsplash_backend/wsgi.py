c77ba1822dabc2e5acfd242a5f534cfe5411c9af
@_@ <Ibtehaz.shawon@gmail.com>
2017-02-08 05:26:29 +0600

    wsgi file changed

--- unsplash_backend/wsgi.py
+++ unsplash_backend/wsgi.py
@@ -10,12 +10,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.template.backends import django
 from whitenoise.django import DjangoWhiteNoise
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unsplash_backend.settings")
 
 application = get_wsgi_application()
-application = django.core.handlers.wsgi.WSGIHandler()
 application = DjangoWhiteNoise(application)
