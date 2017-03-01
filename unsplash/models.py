a6503d16f2d70335231d72f31fb9505ed8408b09
Asif Ibtehaz <asif.ibtehaz@gagagugu.com>
2017-03-01 19:58:24 +0600

    created a database model for curated table

--- unsplash/models.py
+++ unsplash/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 import uuid
+from mx.DateTime import Timezone
 
 from django.core.validators import RegexValidator
 from django.db import models
@@ -54,3 +55,24 @@ class DeviceData(models.Model):
     device_height = models.CharField(max_length=100, null=False)
     device_width = models.CharField(max_length=100, null=False)
     unique_id = models.CharField(max_length=100, unique=True, default=uuid.uuid4, editable=False)
+
+
+
+#Curated List
+class CuratedList(models.Model):
+    curated_id = models.CharField(max_length=100, primary_key=True, unique=True)
+    curated_title = models.CharField(max_length= 200, blank=False, null=False)
+    curated_description = models.CharField(max_length=500, blank=False, null=False)
+    curated_published = models.DateTimeField('date published', null=False)
+    curated_is_curated = models.CharField(max_length=30, null=False, default=False)
+    curated_total_photos = models.CharField(max_length=4, null=False, default=0)
+    curated_share_key = models.CharField(max_length=50, null=False, default="null")
+
+    curated_cover_photo = models.ForeignKey('Photo', null=False)
+
+    curated_user_id = models.CharField(max_length=50, null=False, blank=False)
+    curated_user_name = models.CharField(max_length=30, null=False, blank=False)
+    curated_profile_image_small = models.URLField(null=True, default="none")
+    curated_profile_image_large = models.URLField(null=True, default="none")
+
+
--- unsplash/urls.py
+++ unsplash/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 
+from unsplash import curated
 from unsplash import index_view
 from unsplash import register
 from . import views
@@ -8,5 +9,6 @@ urlpatterns = [
     url(r'^feed/', views.get_feed, name='getFeed'),
     url(r'^view', views.index, name='index'),
     url(r'^register_phone', register.register_phone, name="register_phone"),
+    url(r'^get_curated', curated.get_curated_list, name="curated"),
     url(r'^$', index_view.original_index, name='original_index'),
 ]
