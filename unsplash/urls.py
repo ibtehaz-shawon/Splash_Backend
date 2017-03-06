7b317ecaa0fb8a2af87cfce9fe38b8dde149c162
Asif Ibtehaz <asif.ibtehaz@gagagugu.com>
2017-03-06 11:49:50 +0600

    added get collection lists API feature

--- unsplash/urls.py
+++ unsplash/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     url(r'^register_phone', register.register_phone, name="register_phone"),
     url(r'^collection', curated.get_curated_list, name="curated"),
     url(r'^add_collection', curated.add_collections_photo, name="curated"),
+    url(r'^get_collection', curated.get_collection, name="curated"),
     url(r'^$', index_view.original_index, name='original_index'),
 ]
